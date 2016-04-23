f2f9cb34103b7126fdf0b45c44b29a1d06c007a1
KeshavJ <keshav.jeewanlall@gmail.com>
2016-04-24 01:03:44 +0200

    added timer

--- SuperMarioSanAndreas/world.cpp
+++ SuperMarioSanAndreas/world.cpp
@@ -6,11 +6,19 @@
 //#include <allegro5/allegro_font.h>	
 using namespace std;
 #define length 1366
-#define width 775
+#define width 770
 
  int main(){
 
 	 al_init();
+
+	 bool done = false, draw = false;
+	 int x = 10, y = 700, movespeed = 5;
+	 int state = NULL;
+	 const float FPS = 60.0;
+	 enum Direction {UP, DOWN, LEFT, RIGHT};
+	 ALLEGRO_COLOR blue = al_map_rgb(44, 117, 255);
+	 int dir = DOWN;
 
 	ALLEGRO_DISPLAY *display;
 	al_set_new_display_flags(ALLEGRO_WINDOWED);
@@ -22,56 +30,58 @@ using namespace std;
 	al_init_primitives_addon();
 	al_install_keyboard();
 
+	ALLEGRO_KEYBOARD_STATE keyState;
+	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS);
 	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
 	
-	bool done = false;
-	int x = 10, y = 10, movespeed = 5;
-	int state = NULL;
-	ALLEGRO_COLOR blue = al_map_rgb(44, 117, 255);
 
 	ALLEGRO_BITMAP *imagewindow = al_load_bitmap("bg.png");
 	ALLEGRO_BITMAP *imagewindowsky = al_load_bitmap("Sky.png");
 	ALLEGRO_BITMAP *imagecar = al_load_bitmap("mcar.png");
 	ALLEGRO_BITMAP *imagecopcar = al_load_bitmap("ccar.png");
 
-	al_draw_bitmap(imagewindowsky, 1, 2, NULL);
-	al_draw_bitmap(imagewindow, 1, 5, NULL);
-	al_draw_bitmap(imagecar, 175, 553, NULL);
-	al_draw_bitmap(imagecopcar, 700, 635, NULL);
+	al_start_timer(timer);
 
 	while (!done)
 	{
 		ALLEGRO_EVENT events;
 		al_wait_for_event(event_queue, &events);
 
-		if (events.type = ALLEGRO_EVENT_KEY_DOWN)
+		if (events.type == ALLEGRO_EVENT_KEY_UP)
 		{
 			switch (events.keyboard.keycode)
 			{
-			case ALLEGRO_KEY_DOWN :
-				y += movespeed;
-				break;
-			case ALLEGRO_KEY_UP:
-				y -= movespeed;
-				break;
-			case ALLEGRO_KEY_RIGHT:
-				x += movespeed;
-				break;
-			case ALLEGRO_KEY_LEFT:
-				x -= movespeed;
-				break;
 			case ALLEGRO_KEY_ESCAPE:
 				done = true;
-				break;
 			}
 		}
-		al_draw_rectangle(x, y, x + 20, y + 20, blue, 2.0);
-		al_flip_display();
-		al_draw_bitmap(imagewindowsky, 1, 2, NULL);
-		al_draw_bitmap(imagewindow, 1, 5, NULL);
-		al_draw_bitmap(imagecar, 175, 553, NULL);
-		al_draw_bitmap(imagecopcar, 700, 635, NULL);
+
+		if (events.type == ALLEGRO_EVENT_TIMER)
+		{
+			al_get_keyboard_state(&keyState);
+			if (al_key_down(&keyState, ALLEGRO_KEY_DOWN))
+				y += movespeed;
+			else if (al_key_down(&keyState, ALLEGRO_KEY_UP))
+				y -= movespeed;
+			else if (al_key_down(&keyState, ALLEGRO_KEY_RIGHT))
+				x += movespeed;
+			else if (al_key_down(&keyState, ALLEGRO_KEY_LEFT))
+				x -= movespeed;
+			draw = true;
+		}
+
+		if (draw)
+		{
+			draw = false;
+			al_draw_rectangle(x, y, x + 20, y + 20, blue, 2.0);
+			al_flip_display();
+			al_draw_bitmap(imagewindowsky, 1, 2, NULL);
+			al_draw_bitmap(imagewindow, 1, 5, NULL);
+			al_draw_bitmap(imagecar, 175, 553, NULL);
+			al_draw_bitmap(imagecopcar, 700, 635, NULL);
+		}
 	}
 	
 
@@ -96,5 +106,6 @@ using namespace std;
 	al_destroy_bitmap(imagewindowsky); 
 	al_destroy_bitmap(imagecar);
 	al_destroy_bitmap(imagecopcar);
+	al_destroy_timer(timer);
 	return 0;
 }
