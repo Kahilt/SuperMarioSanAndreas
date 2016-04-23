7504deb49c84af39ae0302c7503adb2293fbfcd7
KeshavJ <keshav.jeewanlall@gmail.com>
2016-04-23 23:06:47 +0200

    changed background

--- SuperMarioSanAndreas/world.cpp
+++ SuperMarioSanAndreas/world.cpp
@@ -4,8 +4,9 @@
 //#include <allegro5/allegro_ttf.h>
 //#include <allegro5/allegro_font.h>	
 using namespace std;
-#define length 800
-#define width 600
+#define length 1366
+#define width 775
+
  int main(){
 
 	 al_init();
@@ -15,17 +16,25 @@ using namespace std;
 	display = al_create_display(length, width);
 	al_set_window_position(display, 200, 100);
 	al_init_image_addon();
-	ALLEGRO_BITMAP *imagewindow = al_load_bitmap("ylogo114.jpg");
-	ALLEGRO_BITMAP *imagewindowRoad = al_load_bitmap("road-clipart-cliparti1_road-clip-art_07.jpg");
+	ALLEGRO_BITMAP *imagewindow = al_load_bitmap("bg.png");
+	ALLEGRO_BITMAP *imagewindowsky = al_load_bitmap("Sky.png");
+	ALLEGRO_BITMAP *imagemusclecar = al_load_bitmap("mcar.png");
+	ALLEGRO_BITMAP *imagecopcar = al_load_bitmap("ccar.png");
+	
+	
 
-	al_draw_bitmap(imagewindow, 1, 1, NULL);
-	al_draw_bitmap(imagewindowRoad, 1, 450, NULL);
-
+	al_draw_bitmap(imagewindowsky, 1, 2, NULL);
+	al_draw_bitmap(imagewindow, 1, 5, NULL);
+	
+	al_draw_bitmap(imagemusclecar, 175, 553, NULL);
+	al_draw_bitmap(imagecopcar, 700, 635, NULL);
+	
+	
 	//al_init_font_addon();
 	//al_init_ttf_addon();
 
 	//ALLEGRO_FONT *font = al_load_font("emulogic.ttf", 20, NULL);
-	//al_draw_text(font, al_map_rgb(44, 117, 255), width / 2, length/4, ALLEGRO_ALIGN_CENTRE, "KESHAV JEEWANLALL");
+	
 	al_flip_display();
 	al_rest(10);
 	//al_destroy_font(font);
