f4c89c13e88ae5b6360b81355fdd91fa01e4503c
KeshavJ <keshav.jeewanlall@gmail.com>
2016-04-25 12:50:16 +0200

    edited splash

--- SuperMarioSanAndreas/SplashScreens.cpp
+++ SuperMarioSanAndreas/SplashScreens.cpp
@@ -5,24 +5,24 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_font.h>	
 using namespace std;
-#define width 800
-#define length 600
-int main()
+#define width 770
+#define length 1366
+int splash()
 {
 	ALLEGRO_DISPLAY *display;
 	if (!al_init())
 	{
-		al_show_native_message_box(NULL, NULL, NULL, "Unable to initialize allegro %", NULL, NULL);
+		//al_show_native_message_box(NULL, NULL, NULL, "Unable to initialize allegro 5", NULL, NULL);
 			return -1;
 	}
 	display = al_create_display(width, length);
 
 	if (!display)
 	{
-		al_show_native_message_box(display, "Sample Title", "Display Settings", "Unable to display");
+		al_show_native_message_box(display, "Sample Title", "Display Settings", "Unable to display",NULL,NULL);
 	}
 	al_destroy_display(display);
-	return 0;
+	
 
 
 }
