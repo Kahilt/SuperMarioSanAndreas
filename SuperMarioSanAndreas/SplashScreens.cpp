0c5d2a694b66d7a641519d11c1594dc56778ea20
PriyenMunien <214529934@stu.ukzn.ac.za>
2016-04-26 09:08:38 +0200

    Continuation

--- SuperMarioSanAndreas/SplashScreens.cpp
+++ SuperMarioSanAndreas/SplashScreens.cpp
@@ -1,7 +1,7 @@
-#include <allegro5\allegro.h>
-#include <allegro5\allegro_native_dialog.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_native_dialog.h>
 #include <iostream> 
-#include <allegro5\allegro_image.h>
+#include <allegro5/allegro_image.h>
 
 using namespace std;
 
