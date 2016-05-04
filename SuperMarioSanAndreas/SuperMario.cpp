780555a9e2a167b0725a83ddb91be5caa5202f55
Kahilt <kahilt11@gmail.com>
2016-05-04 22:26:18 +0200

    Movement for Mario Initialized

--- SuperMarioSanAndreas/SuperMario.cpp
+++ SuperMarioSanAndreas/SuperMario.cpp
@@ -1,21 +1,33 @@
 #include "SuperMario.h"
 
 
-SuperMario::SuperMario()
+SuperMario::SuperMario(float xPos,float yPos)
 {
 	sourceX = 0;
 	sourceY = 0;
-	alive = false;
+	dead = false;
 	active = false;
 	draw = true;
-	x = 10;
-	y = 10;//starting co-ordinates on (x;y) is (10;10)
+	x = xPos;
+	y = yPos;//starting co-ordinates on (x;y) is (xPos;yPos)
 	moveSpeed = 5;
 	check = 0;
-	dir = DOWN;//the initial direction of Mario is set to down
+	dir = RIGHT;//the initial direction of Mario is set to down
 }
 
 
 SuperMario::~SuperMario()
 {
+}
+
+void SuperMario::startMovement(ALLEGRO_DISPLAY *display)
+{
+	al_install_keyboard();
+	ALLEGRO_KEYBOARD_STATE keyState;
+	ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();// create pointer for keyboard
+	ALLEGRO_TIMER *timer = al_create_timer(1.0 / FPS); //event 1/60 sec, the game will update
+	al_register_event_source(event_queue, al_get_keyboard_event_source()); //stores the keyboard buttons in a queue to be executed
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+	al_register_event_source(event_queue, al_get_display_event_source(display));
+	al_start_timer(timer);
 }
--- SuperMarioSanAndreas/SuperMario.h
+++ SuperMarioSanAndreas/SuperMario.h
@@ -7,14 +7,14 @@ using namespace std;
 class SuperMario
 {
 public:
-	SuperMario();//default constructor
+	SuperMario(float,float);//default constructor
 	~SuperMario();//default destructor
 	int sourceX , sourceY ;//used for cropping the SpriteSheet for animation
 
 	enum DIRECTION{ RIGHT, LEFT, DOWN, UP, NONE1, NONE2 }; //Defines the different states or directions of mario. NONE1=facing right NONE2=facing left	
 
 
-	bool alive ;//used to determine when mario will die
+	bool dead;//used to determine when mario will die
 	bool active; //will help cause the animation ONLY if key is pressed in particular direction
 	bool draw ;//for timer, used for smooth animations
 	float x , y , moveSpeed ; // x and y are starting co-ordinates of Mario, moveSpeed is the increment of the co-ordinates hence will det how fast Mario moves
@@ -22,4 +22,6 @@ public:
 	const float FPS =100; //used for the timer, will help determine the number of times the game will refresh per second (frames/sec)
 	int check; //will record Marios last left or right movement to decide which side he will face after the key is left
 	int dir; //the initial direction of Mario is set to down
+
+	void startMovement(ALLEGRO_DISPLAY *);
 };
