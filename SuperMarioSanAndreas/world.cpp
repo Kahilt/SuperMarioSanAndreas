6e1e894be2abeda46071952d7dcbf39bd7354dc6
Kahilt <kahilt11@gmail.com>
2016-05-08 13:31:55 +0200

    Mario Collision Function Inserted

--- SuperMarioSanAndreas/world.cpp
+++ SuperMarioSanAndreas/world.cpp
@@ -6,6 +6,7 @@
 //#include <allegro5/allegro_font.h>	
 #include <allegro5/allegro_audio.h>
 #include <allegro5/allegro_acodec.h>
+#include <cmath>
 using namespace std;
 #define length 1366 //defining the screen lenght 
 #define width 770  //defining the screen width 
@@ -34,6 +35,34 @@ void drawMulti(first startloop, first endloop, first plusplus,second object[],fi
 
 			}
 		}
+int top, bot, lef, righ;
+bool marioCollide(float x, float y, float ex, float ey, int len, int height, float &moveSpeed, int dir, ALLEGRO_BITMAP *player, ALLEGRO_BITMAP *enemy)
+{
+	if (x+len<ex||x>ex+len||y+height<ey||y>ey+height)
+		return false;
+	else
+	{
+		top = max(y, ey);
+		bot = min(y + height, ey + height);
+		lef= max(x, ex);
+		righ = min(x + len, ex + len);
+		for (int i= top; i < bot; i++)
+		{
+			for (int j = lef; j < righ; j++)
+			{
+				al_lock_bitmap(player, al_get_bitmap_format(player), ALLEGRO_LOCK_READONLY);
+				al_lock_bitmap(enemy, al_get_bitmap_format(enemy), ALLEGRO_LOCK_READONLY);
+				ALLEGRO_COLOR color = al_get_pixel(player, j - x, i - y);
+				ALLEGRO_COLOR color2 = al_get_pixel(enemy, j - ex, i - ey);
+				if (color.a != 0 && color2.a != 0)
+				{
+					return true;//collide
+				}
+			}
+		}
+	}
+}
+
  int main(){//main function
 
 	 al_init();
